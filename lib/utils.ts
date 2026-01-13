6eb89e47027eda954ec1229aa04d3249833175be
suragi <suragi2196@gmail.com>
2026-01-13 23:27:33 +0900

    まん

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,7 @@
-import clsx from 'clsx'
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-export function cn(...inputs: any[]): string {
-  return clsx(...inputs)
+// ClassValue[] を使うことで、any を排除しつつ配列やオブジェクトのクラス指定を許可します
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
-
-export default cn
